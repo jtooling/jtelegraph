2519e20beec9f7dc06998ed30981dea19033f048
Antoine Neveux <antoine.neveux@gmail.com>
2012-12-13 17:25:17 +0100

    Added a BatchIconProvider for using more default icons
    Signed-off-by: Antoine Neveux <antoine.neveux@gmail.com>

--- src/test/java/org/jtelegraph/TestTelegraphQueue.java
+++ src/test/java/org/jtelegraph/TestTelegraphQueue.java
@@ -9,6 +9,7 @@ public class TestTelegraphQueue {
 		final TelegraphQueue queue = new TelegraphQueue();
 		final TelegraphConfig c = new TelegraphConfig();
 		c.enableButton(true);
+		c.setIconProvider(BatchIconProvider.ADDRESS_BOOK);
 		// c.setStopOnMouseOver(true);
 		final Telegraph t = new Telegraph("Test",
 				"Hey! Look at my first test!", c);
